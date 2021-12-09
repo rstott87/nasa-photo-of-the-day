e413a9693bbc9ff635e566b614fb8bd7efaf47c7
rstott87 <rstott87@yahoo.com>
2021-12-09 15:15:38 -0500

    added photo as seperate component

---
+++ src/Photo.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Photo (props) {
+    return (<div> 
+                <img src={props.APOD} alt = "" width="500" height="auto"/> 
+            </div>)
+    }
+export default Photo;    
