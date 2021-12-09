b28f3627966f119a7485d51e0f7f8e07d2d8b3a4
rstott87 <rstott87@yahoo.com>
2021-12-08 19:09:18 -0500

    installed axios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.css";
+const axios = require('axios');
 
+const fetchNasaInfo = () => {
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=Ec0fQGaOpvWcJefHxrVvCHBtJzTju76T0wLZbux8')
+  .then((resp)=> {
+    console.log(resp);
+})
+};
+fetchNasaInfo();
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1> Nasa's Photo of The Day</h1>
     </div>
   );
 }
