65ba1d966f13e60cc6032ad476cb0844f9b36db9
rstott87 <rstott87@yahoo.com>
2021-12-09 15:49:19 -0500

    addded Nasa logo and Explanation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { useEffect } from "react";
 import styled from 'styled-components'
 import "./App.css";
 import Photo from "./Photo";
+import Title from "./Title";
+import Explanation from "./Explanation";
 
 
 const axios = require('axios');
@@ -16,34 +18,35 @@ const WrapperDiv = styled.div`
 
 const BlueH1 = styled.h1 `
     color: royalblue;
+    height: 100px;
+    font-size: 40px;
 `
 
-function Title (props) {
-  return <div>  
-          <div>{props.title}</div>
-  </div>
-  }
-
 function Fetcher (props){
+
   const [APOD, setApod] = useState(" ");
   const [title, setTitle] = useState(" ");
+  const [info, setInfo] = useState(" ");
   useEffect(()=> {  axios.get('https://api.nasa.gov/planetary/apod?api_key=Ec0fQGaOpvWcJefHxrVvCHBtJzTju76T0wLZbux8')
   .then((resp)=> {
     setApod(resp.data.url);
     setTitle(resp.data.title);
+    setInfo(resp.data.explanation)
   })
   .catch(err => console.log(err));
 },[]);
   return( <div>
         <Title title={title}/>
-        <Photo APOD={APOD}/>  
+        <Photo APOD={APOD}/>
+        <Explanation explanation = {info}/>
       </div>)
 }
 
 function App() {
   return (
     <WrapperDiv>
-      <BlueH1> Nasa's Photo of The Day</BlueH1>
+      <BlueH1> Nasa's Photo of The Day<img src ="https://images-na.ssl-images-amazon.com/images/I/6111kA98OPL.png" height = "100px"/></BlueH1>
+      
       <Fetcher/>
     </WrapperDiv>
   );
---
+++ src/Explanation.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Explanation (props) {
+    return (<div> {props.explanation} </div>)
+    }
+export default Explanation;    
